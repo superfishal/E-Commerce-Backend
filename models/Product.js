f48bec8b134456ae71b5870176cdb7696e5a672c
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-06 14:12:41 -0700

    finished models and key relationships

--- models/Product.js
+++ models/Product.js
@@ -16,7 +16,21 @@ Product.init(
       autoIncrement: true,
     },
     product_name: { type: DataTypes.STRING, allowNull: false },
-    price: { type: DataTypes.DECIMAL, allowNull: false },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: { isDecimal: true },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: "10",
+      validate: { isInteger: true },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: { model: "category", key: "id" },
+    },
   },
   {
     sequelize,
